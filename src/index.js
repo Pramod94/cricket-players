cad909f838f0aa87c4f2a203813d6820a43dbdec
pmdm333 <pramoda.m@robosoftin.com>
2021-01-20 12:51:13 +0530

    contains all players

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 var cricketPlayers = require('./cricket-players.json');
-var uniqueRandomAry = require('unique-random-array');
 
 module.exports = {
-    all: cricketPlayers,
-    random: uniqueRandomAry(cricketPlayers.players)
+    all: cricketPlayers.players
 }
